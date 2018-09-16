8f429fbd3776a13e41e0569354a7ba033cc0ca9c
ivg2 <ivg2@illinois.edu>
2018-09-16 17:28:12 -0500

    Finished first three sections but still crashing on syntax error.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
+
 from typing import Tuple
 
 from flask import Flask, jsonify, request, Response
 import mockdb.mockdb_interface as db
 
-app = Flask(__name__)
+app = Flask(isabel-greiner)
 
 
 def create_response(
@@ -26,6 +28,9 @@ def create_response(
     """
     if type(data) is not dict and data is not None:
         raise TypeError("Data should be a dictionary 😞")
+        status = 404
+	message = "There is no user with that id."
+
 
     response = {
         "code": status,
@@ -45,14 +50,26 @@ def create_response(
 def hello_world():
     return create_response({"content": "hello world!"})
 
-
 @app.route("/mirror/<name>")
 def mirror(name):
     data = {"name": name}
     return create_response(data)
 
+@app.rout("/users")
+def get_users():
+    return create_response(db.get("user")
 
-# TODO: Implement the rest of the API here!
+@app.rout("/users/<id>")
+def get_user():
+    return create_response(db.getById(user, id)
+
+@app.rout("/users")
+def get_user_with_team:
+   team = request.args.get('param') 
+   users = db.get("user");
+   usersInTeam = {}
+
+   for k in users.items():
 
 """
 ~~~~~~~~~~~~ END API ~~~~~~~~~~~~
